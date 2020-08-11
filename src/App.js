a969631195aadf99ac3e12b65d13ec36d44723d2
mohammad <m.vahedi93@ymail.com>
2020-08-11 12:16:31 +0430

    I m just using git and I will use it more. this is the End of file No.5 folder

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import HomePage from './pages/homepage/homepage.component';
+import './App.css';
+import {Route , Switch} from 'react-router-dom';
+
+
+
+function App() {
+  return (
+    <div>
+      <Switch>
+         <Route exact path='/' component={HomePage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = ({history}) => (
+    <div className='homepage'>
+        <Directory history={history} />
+    </div>
+)
+export default HomePage; 
